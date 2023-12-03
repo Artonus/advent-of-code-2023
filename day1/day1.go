276a38a9b73fd057d280efc3a604f4b416c664b5
Bartosz Bratuś <artonus1@gmail.com>
2023-12-03 22:52:42 +0100

    day1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -9,6 +9,24 @@ import (
 	"unicode"
 )
 
+var digits = map[string]rune{"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7', "eight": '8', "nine": '9'}
+
+func isDigit(text string, reverse bool) (bool, rune) {
+	for key, digit := range digits {
+		if reverse {
+			if strings.HasSuffix(text, key) {
+				return true, digit
+			}
+		} else {
+			if strings.HasPrefix(text, key) {
+				return true, digit
+			}
+		}
+
+	}
+	return false, 0
+}
+
 func Day1() {
 
 	file, err := os.Open("day1/data.txt")
@@ -30,15 +48,25 @@ func Day1() {
 				start = value
 				break
 			}
+			isDigit, digit := isDigit(line[i:], false)
+			if isDigit {
+				start = digit
+				break
+			}
 		}
-		for i := length - 1; i >= 0; i-- {
-			value := rune(line[i])
+		for j := length - 1; j >= 0; j-- {
+			value := rune(line[j])
 			if unicode.IsDigit(value) {
 				end = value
 				break
 			}
+			isDigit, digit := isDigit(line[:j+1], true)
+			if isDigit {
+				end = digit
+				break
+			}
 		}
-		//fmt.Printf("start: %s end: %s \n", string(start), string(end))
+		fmt.Printf("start: %s end: %s \n", string(start), string(end))
 		//fmt.Sprintf("%s%s", start, end)
 		intVal, err := strconv.Atoi(strings.Join([]string{string(start), string(end)}, ""))
 		if err != nil {
